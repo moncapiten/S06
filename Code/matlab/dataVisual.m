da6c0771b5b98278f49d76d78434bbfa6ad65069
moncapiten <moncapiten@gmail.com>
2024-11-17 15:44:35 +0100

    just leaving after a little test, bye

--- Code/matlab/dataVisual.m
+++ Code/matlab/dataVisual.m
@@ -1,5 +1,5 @@
 dataPosition = '../../Data/';
-filename = 'data019';
+filename = 'data008';
 
 
 % data import and creation of variance array
@@ -10,18 +10,19 @@ vi = rawData(:, 2);
 vo = rawData(:, 3);
 
 
-plot(tt, vi, 'o', Color = '#0027BD');
+plot(tt, vo, 'o', Color = '#0027BD');
 hold on
-plot(tt, vo, 'v', Color = 'magenta');
+%plot(tt, vo, 'v', Color = 'magenta');
+plot(tt, mean(vo) * sin(50*tt), 'Color', 'red');
 
 grid on
 grid minor
-title('Amlitude and Ottset of input signal');
-legend('ch1', 'ch2', Location= 'ne')
-ylabel('Vi Amplitude [V]')
-xlabel('frequency [Hz]')
+title('Amplified photodiode voltage');
+%legend('', Location= 'ne')
+ylabel('Voltage [V]')
+xlabel('Time [s]')
 
-hold ott
+hold off
 
 
 
